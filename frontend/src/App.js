0f68bbf07c71a23a9930516b74f19dbf1a7ee3e8
Caden <63772723+cadencjk@users.noreply.github.com>
2023-11-11 03:38:09 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
+
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Collaboration from './pages/Collaboration';
